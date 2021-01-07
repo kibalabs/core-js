4ebd724891c362cc1d7fc32a1906d222bc71d32a
Krishan Patel <krishan711@gmail.com>
2021-01-07 13:59:59 +0000

    .

--- src/services/serviceClient.ts
+++ src/services/serviceClient.ts
@@ -10,11 +10,13 @@ export class RequestData {
 
 // TODO(krishan711): should this be an interface?
 export class ResponseData {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public static fromObject = <T extends ResponseData>(obj: Record<string, unknown>): T => {
-    throw new Error();
-  }
+  // public static fromObject = <T extends ResponseData>(obj: Record<string, unknown>): T => {
+  //   throw new Error();
+  // }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Constructor<T = Record<string, unknown>> = new (...args: any[]) => T;
 
 export class ServiceClient {
   protected requester: Requester;
@@ -25,7 +27,7 @@ export class ServiceClient {
     this.baseUrl = baseUrl;
   }
 
-  protected makeRequest = async <ResponseType extends ResponseData>(method: RestMethod, path: string, request?: RequestData, responseClass?: ResponseType): Promise<ResponseType> => {
+  protected makeRequest = async <ResponseType extends ResponseData>(method: RestMethod, path: string, request?: RequestData | undefined, responseClass?: Constructor<ResponseType> | undefined): Promise<ResponseType> => {
     const url = `${this.baseUrl}/${path}`;
     const response = await this.requester.makeRequest(method, url, request?.toObject());
     // @ts-ignore
