e000e755bf48121e20e04877380bef53b430febf
Barakat Ajadi <37664545+Ajadi-Abiola@users.noreply.github.com>
2022-06-09 17:02:13 +0100

    KIBA-1499: Add dateToRelativeString and dateToRelativeShortString to dateUtil (#24)

--- src/util/dateUtil.ts
+++ src/util/dateUtil.ts
@@ -53,6 +53,77 @@ export const addDays = (date: Date, days: number): Date => {
   return newDate;
 };
 
+export const dateToRelativeString = (date: Date): string => {
+  const seconds = Math.floor((new Date().valueOf() - date.valueOf()) / 1000);
+  let intervalType: string;
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = 'year';
+  } else {
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      intervalType = 'month';
+    } else {
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = 'day';
+      } else {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+          intervalType = 'hour';
+        } else {
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = 'minute';
+          } else {
+            interval = seconds;
+            intervalType = 'second';
+          }
+        }
+      }
+    }
+  }
+  if (interval > 1 || interval === 0) {
+    intervalType += 's';
+  }
+  return `${interval} ${intervalType} ago`;
+};
+
+export const dateToRelativeShortString = (date: Date): string => {
+  const seconds = Math.floor((new Date().valueOf() - date.valueOf()) / 1000);
+  let intervalType: string;
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = 'y';
+  } else {
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      intervalType = 'm';
+    } else {
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = 'd';
+      } else {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+          intervalType = 'h';
+        } else {
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = 'm';
+          } else {
+            interval = seconds;
+            intervalType = 's';
+          }
+        }
+      }
+    }
+  }
+  return `${interval}${intervalType} ago`;
+};
+
 // export const guessTimezone = (): string => {
 //   if (Intl?.DateTimeFormat()?.resolvedOptions()?.timeZone) {
 //     return Intl.DateTimeFormat().resolvedOptions().timeZone;
