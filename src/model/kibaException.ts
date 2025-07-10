0147f1bd215150e372075f7646e533e66c99f762
Krishan Patel <krishan711@gmail.com>
2025-07-10 10:45:07 +0100

    Fiddle: add fields to KibaException (#44)

--- src/model/kibaException.ts
+++ src/model/kibaException.ts
@@ -1,10 +1,14 @@
 
 export class KibaException extends Error {
   public statusCode: number;
+  public exceptionType: string;
+  public fields: Record<string, string>;
 
-  public constructor(message: string, statusCode?: number) {
+  public constructor(message: string, statusCode?: number, exceptionType?: string, fields?: Record<string, string>) {
     super(message);
     this.name = this.constructor.name;
     this.statusCode = statusCode || 500;
+    this.exceptionType = exceptionType || 'KibaException';
+    this.fields = fields || {};
   }
 }
